ee3ccfd719e39191358f043a8b775f851ea9731a
Sorata <sorataoshino@gmail.com>
2022-12-13 12:14:19 +0100

    0.1
    0.1

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Interactor : MonoBehaviour
+{
+    [SerializeField]
+    float radius;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_Position", transform.position);
+        Shader.SetGlobalFloat("_Radius", radius);
+    }
+}
